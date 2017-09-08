ba5cdb90e7aa521b9860bc4ad2440e0c0f53a815
LM <276063276@qq.com>
2017-09-08 18:30:19 +0800

    初始化

---
+++ src/test/java/com/example/bootapi/DemoApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.bootapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.bootapi.repository.AccountRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Autowired
+	AccountRepository accountRepository;
+	
+	@Test
+	public void contextLoads() {
+		System.out.println(accountRepository.count());
+	}
+
+}
